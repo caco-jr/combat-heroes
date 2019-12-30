077836ff5afef43e30395d9fc1d6fc856a07bae3
Carlos Costa <jr.carlosad@gmail.com>
2019-12-30 20:22:16 -0300

    feat: Preparando componente de lista de cards

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,22 +2,25 @@ import React from 'react';
 
 import Header from '../../components/Header';
 import Preview from './components/Preview';
-import Card from '../../components/Card';
+import Card from '../../components/Card/Default';
 import Search from './components/Search';
+import { CardList } from '../../components/Card/List/index.style';
 
 const Home = () => (
   <>
     <Header />
     <Preview />
     <Search />
-    {[...Array(6)].map((item, index) => (
-      <Card
-        key={index}
-        id="70"
-        name="Batman"
-        imageURL="https://www.superherodb.com/pictures2/portraits/10/100/639.jpg"
-      />
-    ))}
+    <CardList>
+      {[...Array(6)].map((item, index) => (
+        <Card
+          key={index}
+          id="70"
+          name="Batman"
+          imageURL="https://www.superherodb.com/pictures2/portraits/10/100/639.jpg"
+        />
+      ))}
+    </CardList>
   </>
 );
 
