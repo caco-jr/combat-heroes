de4d643c97228dfa4675b2d13f3ca28ebb5ec91f
Carlos Costa <jr.carlosad@gmail.com>
2019-12-30 20:50:10 -0300

    feat: Ajustando _document

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,4 +1,9 @@
-import Document from 'next/document';
+import Document, {
+  Head,
+  Main,
+  NextScript,
+  DocumentContext
+} from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
@@ -25,5 +30,61 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal();
     }
+  }
+
+  render() {
+    return (
+      <html lang="en">
+        <Head>
+          <meta
+            name="viewport"
+            content="width=device-width,initial-scale=1,viewport-fit=cover"
+          />
+
+          <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+
+          <link
+            href="https://cdn.rawgit.com/filipelinhares/ress/master/dist/ress.min.css"
+            rel="stylesheet"
+          />
+          <link rel="manifest" href="/static/manifest/manifest.json" />
+          <link rel="manifest" href="manifest.webmanifest" />
+          <link
+            rel="apple-touch-icon"
+            sizes="48x48"
+            href="/static/manifest/launcher-icon.png?v=1.0.0"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="96x96"
+            href="/static/manifest/launcher-icon@2x.png?v=1.0.0"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="192x192"
+            href="/static/manifest/launcher-icon@4x.png?v=1.0.0"
+          />
+          <meta
+            name="apple-mobile-web-app-status-bar-style"
+            content="default"
+          />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+
+          <script
+            async
+            src="https://cdn.jsdelivr.net/npm/pwacompat@2.0.9/pwacompat.min.js"
+            integrity="sha384-VcI6S+HIsE80FVM1jgbd6WDFhzKYA0PecD/LcIyMQpT4fMJdijBh0I7Iblaacawc"
+            crossOrigin="anonymous"
+          />
+
+          {this.props.styleTags}
+        </Head>
+
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
   }
 }
