e53ac308fafe1ea1e9e19c134abbe615fc83c945
Carlos Costa <jr.carlosad@gmail.com>
2019-12-30 18:21:15 -0300

    feat: Criando Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Header = () => {
+  return <header>Combat Heroes</header>;
+};
+
+export default Header;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
+import Header from '../components/Header';
 
 const Title = styled.h1`
   font-size: 50px;
   color: ${({ theme }) => theme.colors.primary};
 `;
 
-export default () => <Title>Hello World!</Title>;
+export default () => (
+  <>
+    <Header />
+    <Title>Hello World!</Title>
+  </>
+);
