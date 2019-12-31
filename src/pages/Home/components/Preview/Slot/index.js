92274dcd2113c5a70fbf436a11e970a74e007d36
Carlos Costa <jr.carlosad@gmail.com>
2019-12-30 22:24:08 -0300

    feat: Mock de preview

---
+++ src/pages/Home/components/Preview/Slot/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { PreviewSlotWrapper } from './index.style';
+
+const PreviewSlot = ({ name }) => {
+  return <PreviewSlotWrapper>{name}</PreviewSlotWrapper>;
+};
+
+export default PreviewSlot;
