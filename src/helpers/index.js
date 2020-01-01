62e87ec63fe76ddc21b892a9085c4bd68205a98a
Carlos Costa <jr.carlosad@gmail.com>
2020-01-01 20:07:22 -0300

    feat: Acrescentando novos itens para Mais Populares

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -2,3 +2,6 @@ export const isServer = typeof window === 'undefined';
 
 export const camelCased = myString =>
   myString.replace(/(-|\_)([a-z])/g, g => g[1].toUpperCase());
+
+const randomIntFromInterval = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
