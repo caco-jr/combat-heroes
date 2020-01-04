c1f7fb86d21687392656cd99449a36d025dae9db
Carlos <jr.carlosad@gmail.com>
2020-01-03 21:04:56 -0300

    feat: Aplicando teste no componente Search

--- src/pages/Home/components/Search/index.js
+++ src/pages/Home/components/Search/index.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { SearchWrapper, SearchInput } from './index.style';
 
 const Search = () => {
+  const [value, setValue] = useState('');
+
+  const handleChange = event => {
+    setValue(event.target.value);
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+  };
+
   return (
-    <SearchWrapper>
-      <SearchInput name="search" />
+    <SearchWrapper onSubmit={handleSubmit}>
+      <SearchInput name="search" value={value} onChange={handleChange} />
     </SearchWrapper>
   );
 };
--- src/pages/Home/components/Search/index.test.js
+++ src/pages/Home/components/Search/index.test.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import renderer from 'react-test-renderer';
+import 'jest-styled-components';
 
+import { SearchWrapper, SearchInput } from './index.style';
 import Search from './index';
 
 describe('With Enzyme', () => {
@@ -9,6 +11,35 @@ describe('With Enzyme', () => {
     const wrapper = shallow(<Search />);
 
     expect(wrapper.exists()).toBe(true);
+  });
+
+  it('should input exists', () => {
+    const wrapper = shallow(<Search />);
+
+    expect(wrapper.find(SearchInput).exists()).toBe(true);
+  });
+
+  it('user text is echoed', () => {
+    const wrapper = shallow(<Search />);
+
+    wrapper
+      .find(SearchInput)
+      .simulate('change', { target: { value: 'Hello' } });
+
+    expect(wrapper.find(SearchInput).props().value).toEqual('Hello');
+  });
+
+  it('when the form is submitted the event is cancelled', () => {
+    const wrapper = shallow(<Search />);
+    let prevented = false;
+
+    wrapper.find(SearchWrapper).simulate('submit', {
+      preventDefault: () => {
+        prevented = true;
+      }
+    });
+
+    expect(prevented).toBe(true);
   });
 });
 
