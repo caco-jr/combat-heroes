88f4d498094b464137fa803743d1c6abfd60767b
Carlos <jr.carlosad@gmail.com>
2020-01-03 21:52:08 -0300

    feat: Aplicando testes para MostPopular

--- src/pages/Home/components/MostPopular/index.test.js
+++ src/pages/Home/components/MostPopular/index.test.js
@@ -1,14 +1,51 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import renderer from 'react-test-renderer';
+import 'jest-styled-components';
+import Card from '../../../../components/Card/Default';
 
 import MostPopular from './index';
 
+const mockCharacterObject = {
+  id: 39,
+  name: 'Batman',
+  image: {
+    url: ''
+  }
+};
+
+const mockCharacterList = Array(9).fill(mockCharacterObject);
+
 describe('With Enzyme', () => {
-  //   it('Should have ', () => {
-  //     const app = shallow(<MostPopular data={{ data: handleData() }} />);
-  //     expect(app.find('Card')).toEqual(9);
-  //   });
+  it('renders', () => {
+    const wrapper = shallow(<MostPopular data={mockCharacterList} />);
+
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should return the default empty array when there is no data to map through', () => {
+    const wrapper = shallow(<MostPopular />);
+
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should have cards of character', () => {
+    const wrapper = shallow(<MostPopular data={mockCharacterList} />);
+
+    expect(wrapper.find(Card)).toHaveLength(mockCharacterList.length);
+  });
+
+  it('should not break without characters', () => {
+    const wrapper = shallow(<MostPopular />);
+
+    expect(wrapper.find(Card)).toHaveLength(0);
+  });
+
+  it('should not break with empty array', () => {
+    const wrapper = shallow(<MostPopular data={[]} />);
+
+    expect(wrapper.find(Card)).toHaveLength(0);
+  });
 });
 
 describe('With Snapshot Testing', () => {
